25440e0f0a28e812a19e45fe06dbb77248396e87
Michiel de Jong <michiel@ripple.com>
2018-01-12 12:42:25 +0100

    draft 2

--- index.js
+++ index.js
@@ -2,35 +2,16 @@ const EventEmitter2 = require('eventemitter2')
 const IlpPacket = require('ilp-packet')
 const http = require('http')
 
-
-// Convert in the opposite direction
-function oerToHttp(data) {
-  const packet = IlpPacket.deserializeIlpPacket(data)
-  return {
-    headers: {
-      'ILP-Destination':  packet.data.destination,
-      'ILP-Condition':    packet.data.executionCondition.toString('base64'),
-      'ILP-Expiry':       packet.data.expiresAt.toISOString(),
-      'ILP-Amount':       packet.data.amount,
-      'ILP-Fulfillment':  packet.data.fulfillment
-    },
-    body:                 packet.data.data
-  }
-}
-
 class Plugin extends EventEmitter2 {
   constructor (opts) {
     super()
     this.opts = opts
   }
 
-
   connect () {
     this.server = http.createServer((req, res) => {
       let chunks = []
-      res.on('data', (chunk) => {
-      	chunks.push(chunk)
-      })
+      res.on('data', (chunk) => { chunks.push(chunk) })
       res.on('end', () => {
         // Convert from ilp-packet object field names described in:
         // https://github.com/interledger/rfcs/blob/de237e8b9250d83d5e9d9dec58e7aca88c887b57/0000-ilp-over-http.md#request
@@ -42,19 +23,18 @@ class Plugin extends EventEmitter2 {
             executionCondition: Buffer.from(req.headers['ILP-Condition'], 'base64'),
             expiresAt:          new Date(req.headers['ILP-Expiry']),
             amount:             req.headers['ILP-Amount'],
-            fulfillment:        req.headers['ILP-Fulfillment'],
-            data:               Buffer.concat(chunks))
-          })
+            data:               Buffer.concat(chunks)
+          }))
         }).then(response => {
           const obj = IlpPacket.deserializeIlpPacket(response)
           switch (obj.type) {
-            case 13:
+            case IlpPacket.Type.TYPE_ILP_FULFILL:
               res.writeHead(200, {
                 'ILP-Fulfillment': obj.data.fulfillment,
               })
               res.end(obj.data.data)
               break
-            case 14:
+            case IlpPacket.Type.TYPE_ILP_REJECT:
               res.writeHead(400, {
                 'ILP-Error-Code': obj.data.code,
                 'ILP-Error-Name': obj.data.name,
@@ -87,17 +67,50 @@ class Plugin extends EventEmitter2 {
       this._connected = false
       this.emit('disconnect')
       resolve()
-    })
+    }))
   }
   isConnected () { return this._connected }
 
-  sendData (data) {
-    const obj = oerToHttp(data)
+  sendData (packet) {
+    const obj = IlpPacket.deserializeIlpPrepare(packet)
     return fetch(this.opts.peerUrl, {
       method: 'POST',
-      body:Promise.resolve(this.mirror._dataHandler ? this.mirror._dataHandler(data) : null) }
+      headers: {
+        'ILP-Destination':  obj.destination,
+        'ILP-Condition':    obj.executionCondition.toString('base64'),
+        'ILP-Expiry':       obj.expiresAt.toISOString(),
+        'ILP-Amount':       obj.amount,
+      },
+      body:                 obj.data
+    }).then(res => {
+      if (res.status === 200) {
+        return IlpPacket.serializeIlpFulfill({
+          fulfillment:        req.headers['ILP-Fulfillment'],
+          data: res.body.buffer()
+        })
+      } else {
+        return IlpPacket.serializeIlpReject({
+          code:          req.headers['ILP-Error-Code'],
+          name:          req.headers['ILP-Error-Name'],
+          triggeredBy:   req.headers['ILP-Error-Triggered-By'],
+          triggeredAt:   req.headers['ILP-Error-Triggered-At'],
+          message:       req.headers['ILP-Error-Message'],
+          data: res.body.buffer()
+        })
+      }
+    }).catch(err => {
+      return IlpPacket.serializeIlpReject({
+        code:          'P00',
+        name:          'plugin bug',
+        triggeredBy:   'ilp-plugin-http',
+        triggeredAt:   new Date(),
+        message:       err.message,
+        data: Buffer.from([])
+      })
+    })
+  }
+
   registerDataHandler (handler) { this._dataHandler = handler }
   deregisterDataHandler (handler) { delete this._dataHandler }
 }
-Plugin.version = 2
-module.exports = Plugin
+Plugin.version = module.exports = Plugin
